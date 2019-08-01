bfed0ee2c3defa8adaf3e21cb7b8e0214a846ef7
Sebastian Kaczmarek <sebos.k10@gmail.com>
2019-08-01 23:01:30 +0200

    Add movies validation schema

---
+++ app/routes/validation-schemas/movies-schema.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Joi = require('joi');
+
+module.exports = {
+    fetchAll: {
+        params: {},
+        body: {}
+    },
+    create: {
+        params: {},
+        body: {
+            title: Joi.string().required().error(new Error('movieId is invalid')),
+            type: Joi.string().allow(['movie', 'series', 'episode']).optional().error(new Error('type is invalid')),
+            year: Joi.number().min(1895).optional().error(new Error('year is invalid')),
+            plot: Joi.string().allow(['short', 'full']).optional().error(new Error('plot is invalid'))
+        }
+    }
+};
